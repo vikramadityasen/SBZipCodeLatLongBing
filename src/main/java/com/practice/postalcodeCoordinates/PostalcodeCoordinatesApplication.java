85d0aa40b2e779da2e59bf4ce86f3e70f6aed01f
Vikram Aditya Sen <sen.vikramadityasen111@gmail.com>
2020-07-24 10:44:52 +0530

    initial skeleton commit
    initial skeleton commit

---
+++ src/main/java/com/practice/postalcodeCoordinates/PostalcodeCoordinatesApplication.java
@@ -0,0 +1,13 @@
+package com.practice.postalcodeCoordinates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostalcodeCoordinatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostalcodeCoordinatesApplication.class, args);
+	}
+
+}
